529a9737079a5d1d5ee5bb4cd3db86ce61581257
IfzaRasool <ifzarasoolarain@gmail.com>
2022-03-22 00:19:09 +0300

    fix linter errors

--- modules/Date.js
+++ modules/Date.js
@@ -1,4 +1,4 @@
-import { DateTime } from './luxon.js';
+import { DateTime } from '../node_modules/luxon/luxon.js';
 
 const time = document.querySelector('.time');
 export const displayDate = () => {
